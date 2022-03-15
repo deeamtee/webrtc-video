4cfe5e66fff8149c625f582f445d3345702db715
Sergey Konstantinov <sergey.konstantinov@raiffeisen.ru>
2022-03-15 22:53:32 +0300

    change port

--- server/script/index.js
+++ server/script/index.js
@@ -2,12 +2,18 @@ const express = require('express');
 const http = require('http');
 const app = express();
 const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
 
-const PORT = 3010;
+const PORT = 5001;
 
 app.get('/', (req, res) => {
     res.send({data: 'ok'})
   });
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+});
 
 server.listen(PORT, () => {
   console.log(`listening on ${PORT}`);
--- src/Components/App/App.tsx
+++ src/Components/App/App.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import Video from '../Video/Video';
+import io from "socket.io-client"
+
+const socket = io.connect('http://localhost:5001')
 
 const App = () => {
+    var socket = io();
     return (
         <Video />
     );
